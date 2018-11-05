6e6ff0342f711349d450369088d9c2f1fe977d0e
Pankaj Garg <pankaj@solana.com>
2018-11-05 18:54:54 +0000

    fix build errors

--- src/sgx/signing/build.sh
+++ src/sgx/signing/build.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 echo --- Build
 (
   set -x
-  openssl genrsa -out temp_priv_key.pem -3 3072
-  openssl rsa -in private_key.pem -pubout -out temp_pub_key.pem
+  openssl genrsa -out "$pwd"/temp_priv_key.pem -3 3072
+  openssl rsa -in "$pwd"/temp_priv_key.pem -pubout -out "$pwd"/temp_pub_key.pem
   make LIBS_PATH="$pwd"/libs OUT="$pwd"/dist PRIV_KEY="$pwd"/temp_priv_key.pem PUB_KEY="$pwd"/temp_pub_key.pem
 )
