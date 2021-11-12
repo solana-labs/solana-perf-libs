c9d41cf24d1bda2e37a505fdf7ff3ae3d6f6c0af
Ryan Leung <ryan.leung@solana.com>
2021-11-12 12:25:45 -0500

    Remove sm_37 and sm_50 kernel compiles to reduce library size

--- src/gpu-common.mk
+++ src/gpu-common.mk
@@ -1,6 +1,6 @@
 NVCC:=nvcc
 GPU_PTX_ARCH:=compute_35
-GPU_ARCHS?=sm_37,sm_50,sm_61,sm_70,sm_75,sm_80
+GPU_ARCHS?=sm_61,sm_70,sm_75,sm_80
 HOST_CFLAGS:=-Wall -Werror -fPIC -Wno-strict-aliasing
 GPU_CFLAGS:=--gpu-code=$(GPU_ARCHS),$(GPU_PTX_ARCH) --gpu-architecture=$(GPU_PTX_ARCH)
 
