12fa738d421392eb66b2f8b7d5d871a04ee8e380
joeaba <77398477+joeaba@users.noreply.github.com>
2021-11-23 18:36:13 +0530

    Update gpu-common.mk
    GPU_PTX_ARCH:=compute_60
    GPU_ARCHS?=sm_61,sm_70,sm_75,sm_80,sm_86

--- src/gpu-common.mk
+++ src/gpu-common.mk
@@ -1,6 +1,8 @@
 NVCC:=nvcc
-GPU_PTX_ARCH:=compute_35
-GPU_ARCHS?=sm_37,sm_50,sm_61,sm_70
+#GPU_PTX_ARCH:=compute_35
+#GPU_ARCHS?=sm_37,sm_50,sm_61,sm_70
+GPU_PTX_ARCH:=compute_60
+GPU_ARCHS?=sm_61,sm_70,sm_75,sm_80,sm_86
 HOST_CFLAGS:=-Wall -Werror -fPIC -Wno-strict-aliasing
 GPU_CFLAGS:=--gpu-code=$(GPU_ARCHS),$(GPU_PTX_ARCH) --gpu-architecture=$(GPU_PTX_ARCH)
 
